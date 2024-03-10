8774fa3a4c0bb3d178b8383f454139a98453a42c
SohelShahid117 <sohelshahid09@gmail.com>
2024-03-10 23:25:52 +0600

    complete 63 min

--- basic.js
+++ basic.js
@@ -19,6 +19,8 @@ console.log(name,typeof name);
 console.log(nmbr,typeof nmbr);
 console.log(std,typeof std);
 
+console.log(`-------------------------------`)
+
 let z = x;
 z = 10;
 console.log(x);
@@ -33,7 +35,10 @@ console.log(nmbr)
 console.log(nmbr2);
 console.log(nmbr)
 
+console.log(`-------------------------------`)
 // let std2 = std;
+
+//spread object
 let std2 = {...std};
 console.log(std)
 console.log(std2)
@@ -41,6 +46,8 @@ console.log(std2)
 std2.id = 0902117;
 console.log(std);
 console.log(std2);
+
+console.log(`-------------------------------`)
 
 //object destructuring
 const {id,name22,age} = std2
@@ -53,6 +60,8 @@ const {id:amarId,name:amarNam,age:amarAge} = std2;
 console.log(amarId)
 console.log(amarNam)
 console.log(amarAge)
+
+console.log(`-------------------------------`)
 
 
 
@@ -76,6 +85,8 @@ const value = {};
 console.log(value);
 console.log(!!value);//truthy or falsy value check
 
+console.log(`-------------------------------`)
+
 const number = [7,89,99,87,56];
 const number2 = [];
 for(num of number){
@@ -89,7 +100,9 @@ number.push(99);
 number2.push(199);
 
 console.log(number)
-console.log(number2)
+console.log(number2);
+
+console.log(`-------------------------------`)
 
 // number2=number;
 
@@ -109,6 +122,8 @@ b.push(909)
 console.log(a)
 console.log(b)
 
+console.log(`-------------------------------`)
+
 
 //destructuring
 
@@ -117,3 +132,54 @@ console.log(p);
 console.log(q);
 console.log(r);
 console.log(s);
+
+console.log(`-------------------------------`)
+
+const person = {
+    name : 'Sohel',
+    age :32,
+    numbrrr :[1,2,34,5,67,78],
+    isDeveloper:true,
+    nJoyNusiness : true,
+    address:{
+        zilla : 'chattogram',
+        subZila:'Rangunia'
+    }
+};
+const {name:personerNam,age:personerAge,...others}=person;
+console.log(personerAge)
+console.log(personerNam)
+console.log(others);
+
+console.log(`-------------------------------`)
+
+const {address} =person;
+console.log(address)
+
+const {zilla,subZila} = address;
+console.log(zilla)
+console.log(subZila)
+
+console.log(`-------------------------------`)
+
+console.log(address)
+const{address:{zilla:thikana},address:{subZila:upaZila}} = person;
+console.log(thikana)
+console.log(upaZila)
+console.log(`-------------------------------`)
+
+function add(x,y){
+    return x+y;
+}
+console.log(add(2,3));
+
+const sum =(x,y)=>{
+    return x+y
+}
+const result = sum(3,4);
+console.log(result);
+
+console.log(sum(22,33));
+
+const double = (x)=>x*2;
+console.log(double(5));
