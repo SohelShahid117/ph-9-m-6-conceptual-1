464a90bfe7c83637d438fddda108ac0a50081eb5
SohelShahid117 <sohelshahid09@gmail.com>
2024-03-13 06:15:25 +0600

    complete 1:24 min

--- basic.js
+++ basic.js
@@ -187,8 +187,72 @@ const double = (x)=>x*2;
 console.log(double(5));
 
 //array method:
-const numberr = [12,13,24,3,45,56];
+const numberr = [12,13,24,3,45,56,NaN];
 console.log(numberr)
 
-//forEach,find,filter,reduce
+//forEach,for of, find,filter,reduce
 
+console.log(`-------------for of---------------`)
+for(n of numberr){
+    console.log(n);
+}
+
+console.log(`--------------forEach--------------`)
+numberr.forEach((n)=>{
+    console.log(n)
+})
+
+
+console.log(`---------------map-------------`);
+numberr.map((itm)=>{
+    console.log(itm)
+})
+
+//what is the difference between forEach & map
+//map noton array return kore but forEach new array return korte parena
+
+
+const forEachhh = numberr.forEach((itm)=>itm);
+console.log(forEachhh)
+
+
+const mappp = numberr.map((itm)=>itm);
+console.log(mappp)
+
+// console.log(numberr);
+
+const mappp2 = numberr.map((itm)=>itm+5);
+console.log(mappp2)
+
+const findd = numberr.find((x)=>x%2==0);
+console.log('first even number using find method-',findd);
+
+const filtrrr = numberr.filter((x)=>x%2==0);
+console.log('all even number using filter method-',filtrrr)
+
+const filtrrr2 = numberr.filter((x)=>x>500);
+console.log('all number array using filter method match with condition-',filtrrr2)
+
+const filtrrr3 = numberr.filter((x)=>isNaN(x));
+console.log('all number array using filter method match with condition-',filtrrr3)
+
+const filtrrr4 = numberr.filter((x)=>!isNaN(x));
+console.log('all number array using filter method match with condition-',filtrrr4);
+
+// const filtrrr5 = numberr.filter((x)=>x !==isNaN());
+// console.log('all number array using filter method match with condition-',filtrrr5);
+
+console.log(numberr);
+let summm = 0;
+for(let x of numberr){
+    summm = summm + x;
+}
+console.log(summm);
+
+let summm2 = 0;
+for(let x of filtrrr4){
+    summm2 = summm2 + x;
+}
+console.log(summm2);
+
+//1:24
